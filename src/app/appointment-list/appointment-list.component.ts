4924131c28df391dbd4f89ed9b62982e12717158
shrash9 <shrashtiagarwal2020@gmail.com>
2025-04-01 17:47:30 +0530

    installed bootstrap

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/interface';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
+
   newAppointmentTitle: string  = "";
   newAppointmentDate : Date= new Date();
   appointments : Appointment[] = []
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments")
+    
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
@@ -23,8 +31,13 @@ export class AppointmentListComponent {
       this.newAppointmentTitle="";
       this.newAppointmentDate = new Date();
 
-      alert(this.appointments.length)
-    }
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    }      
+  }
+
+  removeAppointment(index:number){
+    this.appointments.splice(index,1)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
   }
 
 }
